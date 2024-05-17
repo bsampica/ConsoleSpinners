a00cc6bef266b1376aa2cd79fc21e9699c0a8a0a
Bryan Sam <bryansampica@hotmail.com>
2024-05-17 16:41:49 -0400

    Code cleanup, and documention in the comments of the code.

--- Program.cs
+++ Program.cs
@@ -11,11 +11,13 @@ namespace SaveFileWatcher
     {
         static void Main(string[] args)
         {
+            // If we want ANSI Support in the console, this is one way to do it
+            // this may not work in all windows consoles or systems.  
             Console.OutputEncoding = System.Text.Encoding.GetEncoding(65001);
             Console.CursorVisible = false;
             Console.Clear();
 
-
+            // FIGLET~  YAy!
             var font = FigletFont.Load("fonts/serifcap.flf");
             AnsiConsole.Write(
                 new FigletText(font, "File Backup")
@@ -24,13 +26,17 @@ namespace SaveFileWatcher
 
             AddLineSpace(1);
 
+            // HACK: We setup a few things so that we can render the spinner directly.
+            // We need a place to send an IRenderable, but we also have to send it the DeltaTime to play
+            // the animation.
+            // This is taken from hacking apart the AnsiConsole.Progress setup, and using a progress 
+            // column to render the spinner.
             var progressTask = new ProgressTask(1, "Gogogo", 100000, true);
             var spinnerList = typeof(Spinner.Known).GetProperties().OrderBy(s => s.Name).ToList();
             var startPosition = Console.GetCursorPosition().Top + 1;
             var spinnerCol = new SpinnerColumn();
 
-
-
+            // Lets wait for key input, pressing the ESC key will break out of the loop and end the program.
             do
             {
                 while (!Console.KeyAvailable)
@@ -40,10 +46,8 @@ namespace SaveFileWatcher
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
 
-            AnsiConsole.WriteLine("");
-            AnsiConsole.WriteLine("");
-            AnsiConsole.WriteLine("");
-            AnsiConsole.WriteLine("");
+            // Add some some extra space below the rendered spinners when the program exits.
+            AddLineSpace(3);
             AnsiConsole.MarkupLine("[bold blue] -- Exiting Program -- [/]");
 
         }
@@ -58,6 +62,11 @@ namespace SaveFileWatcher
 
         private static void RenderSpinnerList(int startPosition, ProgressTask progressTask, List<PropertyInfo> spinnerList, SpinnerColumn spinnerCol)
         {
+            //TODO: There's an exception in here if the console window isn't big enough to 
+            //      render all the columns, or rows, or it resizes at the wrong time
+            //      It probably needs to calculate, rather than have a hardcoded 3 columns
+            //      or use columns / number of spinners to determine the rows.
+
 
             var pos = startPosition;
             var col = 0;
@@ -91,6 +100,8 @@ namespace SaveFileWatcher
 
         private static void RenderSpinner(int cursorLeftPosition, int cursorTopPosition, PropertyInfo spinner, ProgressTask task, SpinnerColumn spinnerColumn)
         {
+            // The exception occures here during SetCursorPosition() - when it's outside the bounds of the window,
+            // Maybe it could write into the buffer instead?
             Console.SetCursorPosition(cursorLeftPosition, cursorTopPosition);
             AnsiConsole.Write($"{spinner.Name} : ");
             spinnerColumn.Spinner = (Spinner)spinner.GetValue(spinner)!;
